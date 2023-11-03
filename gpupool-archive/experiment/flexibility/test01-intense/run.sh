b48637b30b9f6b83922285930ea447766e241635
Irene-Ting <irene0503.ting@gmail.com>
2023-11-03 10:32:30 +0000

    restart

--- gpupool-archive/experiment/flexibility/test01-intense/run.sh
+++
@@ -1,8 +0,0 @@
-python3 02-2-run-fix.py 4-4
-sleep 2000
-python3 03postprocess.py 4-4
-kubectl delete -f ../workload
-sleep 20
-python3 02-1-run-gpu-pool.py
-sleep 2000
-python3 03postprocess.py pool
